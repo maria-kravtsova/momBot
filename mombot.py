2b53efbb4d64fecc6871668d9136fd3bbfa071c5
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-11-18 22:46:04 -0500

    A bit more planning regarding the capabilities of the weather is required.

--- mombot.py
+++ mombot.py
@@ -11,14 +11,30 @@ api = twitter.Api(consumer_key=consumer_key,
 # openweathermap.org API key
 owm = pyowm.OWM(weather_key)
 
+fc = owm.three_hours_forecast('Kalamazoo, US')
+f = fc.get_forecast()
+print(f.get_weathers())
+
 # get observation forecast for today in Kalamazoo, MI
 observation = owm.weather_at_place('Kalamazoo, US')
 weather = observation.get_weather()
+
+# temperature outside.
 temperatures = weather.get_temperature('fahrenheit')
 current_temperature = temperatures.get('temp')
 stringTemperature = str(current_temperature)
+
+# snow
+snow = weather.get_snow()
+print(snow)
+
+
+# rain
+rain = weather.get_rain()
+print(rain)
+
 new_status = 'Current temperature is ' + stringTemperature + ' degrees F.'
 
 # tweet the current temperature
-status = api.PostUpdate(status = new_status)
-print(status.text)
+# status = api.PostUpdate(status = new_status)
+# print(status.text)
